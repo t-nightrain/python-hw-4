390bffbe175bac2fed999fb1ed94cd5cd44f897f
t-nightrain <109236029+t-nightrain@users.noreply.github.com>
2022-08-02 23:10:16 +0300

    twoSum fix, return array of indexes instead print

--- twoSum.py
+++ twoSum.py
@@ -6,11 +6,12 @@ def twoSum(nums: List[int], target: int):
     for i in range(len(nums) - 1):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
-                print([i, j])
-                return
+                return [i, j]
+    return None
 
 
-twoSum([1, 2, 3], 4)
-twoSum([2, 7, 11, 15], 9)
-twoSum([3, 2, 4], 6)
-twoSum([3, 3], 6)
+# print(twoSum([1, 2, 3], 4))
+# print(twoSum([2, 7, 11, 15], 9))
+# print(twoSum([3, 2, 4], 6))
+# print(twoSum([3, 3], 6))
+# print(twoSum([1, 3, 6, 7], 10))
